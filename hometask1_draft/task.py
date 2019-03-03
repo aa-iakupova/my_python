aca5e570b849a724e0843dcd710c2eca24dc298d
Alina Iakupova <alina.yakupova.98@mail.ru>
2019-03-03 23:40:22 +0300

    init commit

--- hometask1_draft/task.py
+++ hometask1_draft/task.py
@@ -1,3 +1,26 @@
-def function(x):
-    x=input()
+def function(text, sep):
+    '''
+    >>> function('ThisIsCamelCased',_)
+    'this_is_camel_cased'
+    
+    >>> function('ThisIsCamelCased',-)
+    'this-is-camel-cased'
+    '''
+    lst=[]
+    for i in text:
+        if text[0]==i:
+            lst.append(i)
+            continue
+        if i.isupper():
+            lst.append(sep)
+            lst.append(i.lower)
+    result=''.join(lst)
+    return result
+            
+print(function('ThisIsCamelCased','_'))
+
+
+#if __name__ == '__main__':
+ #   import doctest
+  #  doctest.testmod()
     
